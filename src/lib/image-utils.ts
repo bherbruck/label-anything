d57612613f2ce5558e1297d7b4259e0716e12f66
bherbruck <brennen.herbruck@gmail.com>
2025-01-30 19:48:06 +0000

    refactor: optimize imports

--- src/lib/image-utils.ts
+++ src/lib/image-utils.ts
@@ -1,32 +1,33 @@
-import * as ort from 'onnxruntime-web'
-import * as tf from '@tensorflow/tfjs'
-import { MODEL_WIDTH, MODEL_HEIGHT } from './types'
+import { tensor as createTensor } from '@tensorflow/tfjs'
+import { type InferenceSession, Tensor } from 'onnxruntime-web'
+import { MODEL_HEIGHT, MODEL_WIDTH } from './types'
 
 export async function generateImageEmbedding(
-  encoderSession: ort.InferenceSession,
+  encoderSession: InferenceSession,
   fileData: File,
-): Promise<ort.Tensor | null> {
+): Promise<Tensor | null> {
   try {
     const inputImage = await createImageBitmap(fileData, {
       resizeWidth: MODEL_WIDTH,
       resizeHeight: MODEL_HEIGHT,
     })
-    const resizedTensor = await ort.Tensor.fromImage(inputImage, {
+    const resizedTensor = await Tensor.fromImage(inputImage, {
       resizedWidth: MODEL_WIDTH,
       resizedHeight: MODEL_HEIGHT,
     })
     const resizeImage = resizedTensor.toImageData()
-    let imageDataTensor = await ort.Tensor.fromImage(resizeImage)
-    const tfTensor = tf
-      .tensor(imageDataTensor.data, imageDataTensor.dims as [number, number, number])
+    let imageDataTensor = await Tensor.fromImage(resizeImage)
+    const tfTensor = createTensor(
+      imageDataTensor.data,
+      imageDataTensor.dims as [number, number, number],
+    )
       .reshape([3, MODEL_HEIGHT, MODEL_WIDTH])
       .transpose([1, 2, 0])
       .mul(255)
 
     // @ts-expect-error
-    imageDataTensor = new ort.Tensor(tfTensor.dataSync(), tfTensor.shape)
+    imageDataTensor = new Tensor(tfTensor.dataSync(), tfTensor.shape)
 
-    ort.env.wasm.numThreads = 1
     const results = await encoderSession.run({
       input_image: imageDataTensor,
     })
